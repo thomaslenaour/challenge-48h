65e08286d2eede79d43d5601d525a86a4157f42d
tlnkorr <lenaourthomas@gmail.com>
2020-05-14 12:28:25 +0200

    backend: fix error weh starting api

--- app/backend/controllers/companiesController.js
+++ app/backend/controllers/companiesController.js
@@ -86,7 +86,49 @@ const register = async (req, res, next) => {
     .json({ companyId: createdCompany.id, email: createdCompany.email, token })
 }
 
-const login = async (req, nes, next) => {}
+const login = async (req, nes, next) => {
+  const { email, password } = req.body
+
+  let user
+  try {
+    user = await User.findOne({ email })
+  } catch (error) {
+    return next(new HttpError('Logging in failed.', 500))
+  }
+
+  if (!user) {
+    return next(new HttpError('Invalid credentials', 403))
+  }
+
+  let isValidPassword = false
+  try {
+    isValidPassword = await bcrypt.compare(password, user.password)
+  } catch (err) {
+    return next(
+      new HttpError(
+        'Could not login, please check your credentials and try again.',
+        500
+      )
+    )
+  }
+
+  if (!isValidPassword) {
+    return next(new HttpError('Invalid credentials', 403))
+  }
+
+  let token
+  try {
+    token = jwt.sign(
+      { userId: user.id, email: user.email },
+      process.env.JWT_KEY,
+      { expiresIn: '1h' }
+    )
+  } catch (err) {
+    return next(new HttpError('Logging up failed, please try again', 500))
+  }
+
+  res.json({ userId: user.id, email: user.email, token })
+}
 
 exports.getCompanies = getCompanies
 exports.getCompany = getCompany

369ea7f72006eb0c7abb603dd57f6193e5abd3ed
tlnkorr <lenaourthomas@gmail.com>
2020-05-14 19:30:27 +0200

    frontend: add map

--- app/frontend/src/pages/MapPage.jsx
+++ app/frontend/src/pages/MapPage.jsx
@@ -1,16 +1,35 @@
-import React, { useEffect } from 'react'
-import {initMap}  from '../services/Map'
+import React, { useState, useEffect } from 'react'
+import { toast } from 'react-toastify'
+import Map from '../components/Map'
+import CompaniesAPI from '../services/CompaniesAPI'
 
 const MapPage = () => {
+  const [companies, setCompanies] = useState([])
+
+  const fetchCompanies = async () => {
+    try {
+      const companies = await CompaniesAPI.fetchCompanies().then(
+        response => response.data.companies
+      )
+      setCompanies(companies)
+    } catch (error) {
+      toast.error('Une erreur est survenue ❌')
+    }
+  }
+
   useEffect(() => {
-    initMap()
+    fetchCompanies()
   }, [])
+
   return (
-    <>
-      <div id="map" style={{ height:"1080px"}}>
-            
-      </div>
-    </>
+    <div className="container">
+      <h2 className="text-center my-5">Map</h2>
+      <Map
+        containerElement={<div style={{ height: `800px`, width: '1000px' }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+        companies={companies}
+      />
+    </div>
   )
 }
 

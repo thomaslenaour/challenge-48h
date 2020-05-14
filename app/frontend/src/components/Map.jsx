2eb4e31188f74f85e9e2c8fbd10ba025306bd023
tlnkorr <lenaourthomas@gmail.com>
2020-05-14 21:23:13 +0200

    frontend: fix map css + add title on hover marker

--- app/frontend/src/components/Map.jsx
+++ app/frontend/src/components/Map.jsx
@@ -6,10 +6,10 @@ const Map = withGoogleMap(props => {
     <div className="d-flex justify-content-center border">
       <GoogleMap
         defaultCenter={{ lat: 44.841211, lng: -0.5646937 }}
-        defaultZoom={13}
+        defaultZoom={12}
       >
-        {props.companies.map(marker => (
-          <Marker position={marker.location} name="Marker" color="red" />
+        {props.companies.map(company => (
+          <Marker position={company.location} title={company.name} />
         ))}
       </GoogleMap>
     </div>

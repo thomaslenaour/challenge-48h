87b3ec6ab63e9cfc312057089d5fd0ee94216319
tlnkorr <lenaourthomas@gmail.com>
2020-05-14 12:00:58 +0200

    backend: add models

---
+++ app/backend/models/company.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const { Schema } = mongoose
+
+const companySchema = Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  address: { type: String, required: true },
+  postal_code: { type: String, required: true },
+  city: { type: String, required: true },
+  masks_stock: { type: Number, required: true },
+  created_at: { type: Number, required: true },
+  updated_at: { type: Number },
+  reservations: [
+    { type: mongoose.Types.ObjectId, required: true, ref: 'Reservation' }
+  ]
+})
+
+companySchema.plugin(uniqueValidator)
+
+module.exports = mongoose.model('Company', companySchema)
---
+++ app/backend/models/reservation.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const { Schema } = mongoose
+
+const reservationSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: String, required: true },
+  masks: { type: Number, required: true },
+  company: { type: mongoose.Types.ObjectId, required: true, ref: 'Company' }
+})
+
+module.exports = mongoose.model('Reservation', reservationSchema)

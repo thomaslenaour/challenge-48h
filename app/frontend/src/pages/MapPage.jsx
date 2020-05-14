d76f6743031c17062943901b67e546b62f7f611f
sarumiishi <43409960+pierreSALMI@users.noreply.github.com>
2020-05-14 15:20:45 +0200

    frontend: add map

--- app/frontend/src/pages/MapPage.jsx
+++ app/frontend/src/pages/MapPage.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import Map  from '../services/Map'
 
 const MapPage = () => {
-  return <h1>Nos partenariats à côté de chez vous !</h1>
+  return (
+    <>
+      <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css" integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ==" crossorigin="" />
+      <div id="map" style={{ height:"1080px"}}>
+            
+      </div>
+    </>
+  )
 }
 
 export default MapPage
